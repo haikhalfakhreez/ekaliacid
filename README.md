8d5dcf00eff33e845a70baa1a9106ff1017a9bf8
Haikhal Fakhreez <ekaliacid@Ekal-MacBook-Pro.local>
2023-04-24 20:53:23 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ekaliacid's Turborepo
---
+++ apps/www/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'ekaliacid',
+  url: 'https://haikhalfakhreez.com',
+  ogImage: 'https://haikhalfakhreez.com/og.jpg',
+  description:
+    'Beautifully designed components built with Radix UI and Tailwind CSS.',
+  links: {
+    twitter: 'https://twitter.com/ekaliacid',
+    github: 'https://github.com/haikhalfakhreez',
+  },
+}
+
+export type SiteConfig = typeof siteConfig
